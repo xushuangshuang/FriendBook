9039f6d0cff2a6ff4e5b33ff28ac072511512a03
xushuangshuang <xushuangshuang1@gmail.com>
2014-12-29 12:20:44 +0800

    修改测试用例并且修改配置

--- src/main/java/com/xu/friendBook/controller/FriendsList.java
+++ src/main/java/com/xu/friendBook/controller/FriendsList.java
@@ -1,16 +1,36 @@
 package com.xu.friendBook.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.xu.friendBook.formModel.Friends;
+import com.xu.friendBook.service.FriendsService;
 
 @Controller
-@RequestMapping("user")
+@RequestMapping("/user")
 public class FriendsList {
 
+	 private FriendsService service;
+	 
+	 @Autowired
+	 public  FriendsList(FriendsService service) {
+		 this.service = service;
+	 }
+	 
 	 @RequestMapping(value = "/list", method = RequestMethod.GET) 
-	 public String friendList() {
-		 return null;
+	 public ModelAndView friendList() {
+		 
+		 Map<String, Friends> modelUser = service.getAllFriendsMap();
+		 
+		 ModelAndView mv = new ModelAndView();
+		 mv.addAllObjects(modelUser);
+		 mv.setViewName("userList");
+		 return mv;
 	 }
 	
 }
--- src/test/java/com/xu/friendBook/controller/test/FriendsListTest.java
+++ src/test/java/com/xu/friendBook/controller/test/FriendsListTest.java
@@ -11,9 +11,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -39,11 +39,10 @@ public class FriendsListTest  {
 
 	@Test
 	public void listTest() throws Exception {
-		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/user/list"))
-				.andExpect(MockMvcResultMatchers.view().name("/user/list"))
-				.andDo(MockMvcResultHandlers.print())
+		MvcResult result = mockMvc.perform(get("/user/list"))
+				.andExpect(view().name("userList"))
 				.andReturn();
 		
-		Assert.assertNotNull(result.getModelAndView().getModel().get("user"));
+		Assert.assertNotNull(result.getModelAndView().getViewName().concat("userList"));
 	}
 }
